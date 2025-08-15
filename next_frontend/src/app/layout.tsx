20a57f4eeff0006ab5536581a80b57ff91e13742
Weston Crewe <westondcrewe@gmail.com>
2025-08-15 14:00:55 -0700

    add analytics

--- next_frontend/src/app/layout.tsx
+++ next_frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      <head
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      </head>
+      <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
